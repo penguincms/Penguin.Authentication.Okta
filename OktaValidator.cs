d9f527f97947f20ce74e94f7399c81489565cdec
AnLoffredo <AnLoffredo@massageenvy.com>
2021-09-04 08:17:51 -0700

    Autocommit
    Autocommit

--- OktaValidator.cs
+++ OktaValidator.cs
@@ -3,13 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Security;
 
 namespace Penguin.Authentication.Okta
 {
-    public static class OktaValidator
+    public class OktaValidator
     {
-        public static bool Validate(string domain, string username, string password)
+        public string SubDomain { get; private set; }
+
+        public OktaValidator(string oktaSubDomain)
         {
+            this.SubDomain = oktaSubDomain;
+        }
+
+        public bool Validate(string username, string password)
+        {
+            return Validate(this.SubDomain, username, password);
+        }
+
+        public static bool Validate(string oktaSubDomain, string username, string password)
+        {
+#if NETFRAMEWORK
+            if (!ServicePointManager.SecurityProtocol.HasFlag(SecurityProtocolType.Tls12))
+            {
+                throw new SecurityException($"{nameof(ServicePointManager)}.{nameof(ServicePointManager.SecurityProtocol)} must have flag {nameof(SecurityProtocolType)}.{nameof(SecurityProtocolType.Tls12)} ({(int)SecurityProtocolType.Tls12}) set in order to connect to OKTA service");
+            }
+#endif
             OktaRequest request = new OktaRequest()
             {
                 Options = new LoginOptions()
@@ -23,7 +42,7 @@ namespace Penguin.Authentication.Okta
 
             JsonClient jc = new JsonClient();
 
-            OktaResponse response = jc.UploadJson<OktaResponse>($"https://{domain}.okta.com/api/v1/authn", request);
+            OktaResponse response = jc.UploadJson<OktaResponse>($"https://{oktaSubDomain}.okta.com/api/v1/authn", request);
 
             return response.Status == "SUCCESS";
         }
